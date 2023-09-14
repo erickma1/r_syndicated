025bbea94d4b199be7252e2779038de6da333c7b
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-09-14 13:39:01 +0000

    Create Navbar and various sections

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import { Link } from "react-router-dom";
-import Navbar from "./Navbar";
+import Appbar from "./Navbar";
+import Mainpage from "./Mainpage";
+import Sidebar from "./Sidebar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export default () => (
-  <div>
-    <Navbar />
-    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-      <div className="jumbotron jumbotron-fluid bg-transparent">
-        <div className="container secondary-color">
-          <h1 className="display-4">Food Recipes</h1>
-          <p className="lead">
-            A curated list of recipes for the best homemade meal and delicacies by Eric.
-          </p>
-          <hr className="my-4" />
-          <Link
-            to="/recipes"
-            className="btn btn-primary"
-            role="button"
-          >
-            View Recipes
-          </Link>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+const Welcome = () => {
+  return (
+    <Container>
+        <Appbar />
+      
+      <Row>
+      <Col sm={8}><Mainpage /></Col>
+      <Col sm={4}><Sidebar /></Col>
+    </Row>
+    </Container>
+  );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Welcome />, document.getElementById("welcome"));
+});
+
+export default Welcome;
