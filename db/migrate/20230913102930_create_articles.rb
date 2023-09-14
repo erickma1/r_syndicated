dae2271ca9b84a7fc3d61acb5fae0d92879e9d5e
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-09-14 12:32:38 +0000

    Update 20230913102930_create_articles.rb

--- db/migrate/20230913102930_create_articles.rb
+++ db/migrate/20230913102930_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.string :headline
-      t.text :main_text
-      t.integer :section
-      t.string :image
+      t.string :headline, null: false
+      t.text :main_text, null: false
+      t.integer :section, null: false
+      t.string :image, default: 'https://raw.githubusercontent.com/do-community/react_rails_recipe/master/app/assets/images/Sammy_Meal.jpg'
 
       t.timestamps
     end
