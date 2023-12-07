9c2e0367967bc1d615b6e44ffb78c49cf89bf92c
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-07 10:17:38 +0000

    Update Navbar.jsx

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -1,54 +1,43 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap'; // Note the change here
 
-function Appbar() {
+const MyNavbar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Home</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">News</Nav.Link>
-            <Nav.Link href="#action2">Business</Nav.Link>
-            <Nav.Link href="#pricing">Sports</Nav.Link>
-            <Nav.Link href="#pricing">Entertainment</Nav.Link>
-            <Nav.Link href="#pricing">World</Nav.Link>
-            <Nav.Link href="#pricing">Technology</Nav.Link>
-            <Nav.Link href="#pricing">Blog</Nav.Link>
-            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown> */}
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">SyndicatedNews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" active>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#">Page 1</Nav.Link>
+            <Nav.Link href="#">Page 2</Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
               type="search"
               placeholder="Search"
-              className="me-2"
               aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+            ></input>
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+          <Nav className="ms-auto">
+            <Nav.Link href="#">
+              <i className="bi bi-person" /> Sign Up
+            </Nav.Link>
+            <Nav.Link href="#">
+              <i className="bi bi-box-arrow-in-right" /> Login
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
-export default Appbar;
+export default MyNavbar;
