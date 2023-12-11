02e0a42fb349689ee7bd897dc06aa11df3f567e8
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-11 05:52:12 +0000

    Update Sidebar.jsx

--- app/javascript/components/Sidebar.jsx
+++ app/javascript/components/Sidebar.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import News from "./sections/News";
+import Frontpicstory from './sections/Frontpicstory'
 
 
 function Sidebar() {
   return (
-    <>
-   <News />
-   
-    </>
+    <Card
+    style={{
+      backgroundColor: '#ffffff',
+      backgroundSize: 'cover',
+      color: 'black',
+      borderRadius: '8px',
+      // boxShadow: '5px 5px 5px #aaaaaa',
+      border: 0,
+    }}
+  >
+    <Card.Body>
+    <Row>
+      <Col sm={6} style={{ color: '#ffffff'}}>
+        <Frontpicstory />
+      </Col>
+      <Col sm={6} style={{ color: '#ffffff'}}>
+        <News />
+      </Col>
+    </Row>
+    </Card.Body>
+    </Card>
   );
 }
 
