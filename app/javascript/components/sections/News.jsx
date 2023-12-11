7d5630d1d19fb141ca9beed452ff4ce1beb24cb6
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-11 05:52:03 +0000

    Update News.jsx

--- app/javascript/components/sections/News.jsx
+++ app/javascript/components/sections/News.jsx
@@ -7,7 +7,7 @@ const News = () => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    const url = "/api/v1/articles/news";
+    const url = "/api/v1/articles/mostread";
     fetch(url)
       .then((res) => {
         if (res.ok) {
@@ -20,43 +20,59 @@ const News = () => {
   }, [navigate]);
 
   const allArticles = articles.map((article) => (
-    <div key={article.id} className="lh-base">
+    <div className="row" key={article.id}>
+      <div className="col-md-7">
       <Link
+      style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '2', textOverflow: 'ellipsis', overflow: 'hidden'}}
         to={`/article/${article.id}`}
         className="text-start link-dark link-offset-3-hover link-underline 
          link-underline-opacity-0 link-underline-opacity-75-hover"
       >
         {article.headline}
       </Link>
-      <hr />
-    </div>
+
+        </div>
+
+        <div className="col-md-5">
+        <Link
+          to={`/article/${article.id}`}
+          className="text-start link-dark link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+        >
+          <img
+            src={article.image}
+            style={{ width: '100%', height: 'auto', margin: '0px, 0px, 3px, 0px' }}
+            className="img-fluid"
+            alt={article.headline}
+          />
+        </Link>
+
+
+        </div>
+        <hr />
+      </div>
+
   ));
 
   const noArticles = (
-    <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
-      <h4>
-        No articles yet. Why not <Link to="/new_article">create one</Link>?
-      </h4>
-    </div>
+    <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">Loading...</div>
   );
 
   return (
-    <>
-      <br />
-      <Card style={{ backgroundColor: '#ffffff', backgroundSize: 'cover', color: 'black', borderRadius: 0, border: 0 }}>
-        <Card.Header>News</Card.Header>
+     
+      <Card style={{ fontSize: '15px', fontWeight: '500', backgroundColor: '#ffffff', backgroundSize: 'cover', color: 'black', borderRadius: 0, border: 0 }}>
+        <Card.Header>Trending</Card.Header>
         <Card.Body>
           {/* <Card.Title>Secondary Element</Card.Title> */}
           {articles.length > 0 ? (
-            <div className="row">{allArticles}</div>
+            <div>{allArticles}</div>
           ) : (
             noArticles
           )}
         </Card.Body>
         {/* <div className="card-footer bg-transparent border-success">More News...</div> */}
       </Card>
-      <br />
-    </>
+    
+    
   );
 };
 
