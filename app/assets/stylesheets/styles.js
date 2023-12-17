4a62b00e2b777c8e5d00ad2db5960bdbf86e1b08
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-17 17:25:33 +0000

    Update styles.js

--- app/assets/stylesheets/styles.js
+++ app/assets/stylesheets/styles.js
@@ -1,11 +1,17 @@
-const styles = {
+export const styles = {
     customButton: {
       backgroundColor: '#0B0C10',
       borderColor: '#45A293',
       color: '#45A293',
       borderRadius: '100px'
+    },
+
+    link: {
+      linkOffset: '2-hover',
+      linkUnderlineOpacity: '75-hover',
+      color: 'red'
     }
   };
   
-  export default styles;
+
   
