fce77a6806f83667f9828f8ca4e3404e8641c699
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-18 21:13:16 +0000

    Update Headlinestory.jsx

--- app/javascript/components/Headlinestory.jsx
+++ app/javascript/components/Headlinestory.jsx
@@ -27,62 +27,31 @@ const Headlinestory = () => {
 
   const articlemain_text = addHtmlEntities(article.main_text);
 
-  const containerStyle = {
-    position: 'relative',
-    maxWidth: '100%', // Ensures the image is responsive
-  };
-
-  const textStyle = {
-    position: 'absolute',
-     // Adjust the vertical position as needed
-    left: '10%', // Adjust the horizontal position as needed
-    bottom: '1%',
-    transform: 'translate(-10%, -10%)', // Center the text within the container
-    color: 'white', // Text color
-    fontSize: '24px', // Adjust the font size as needed
-    fontFamily: 'Source Sans Pro, sans-serif !important',
-    fontWeight: '700',
-  };
-
-  const overlayStyle = {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    width: '100%',
-    height: '30%', // Adjust the height to control the darkness
-    background: 'linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0.8))',
-    pointerEvents: 'none', // Allows interaction with elements beneath the overlay
-  };
-
   return (
-    <div className="">
-      
-
+    <div className="headline-news-image">
       <Card>
+        <Card.Img
+          variant="top"
+          src={article.image}
+          alt="Your Image"
+          className=""
+          // style={{ width: '100%', display: 'block', height: '300px' }}
+        />
 
-        <Card.Img variant="top" src={article.image} alt="Your Image" style={{ width: '100%',  display: 'block', height: '300px' }} />
-  
-        <Link
-            className="link-dark offset-2 link-offset-3-hover link-underline 
-         link-underline-opacity-0 link-underline-opacity-75-hover"
-            to={`/article/${article.id}`}
-          >
-        <Card.ImgOverlay>
-        <div style={overlayStyle}>
-        <Card.Title><div style={textStyle}>{article.headline}</div></Card.Title>
-        </div>
-        
-        </Card.ImgOverlay>
-        
-        
-        <Card.Body>
+        <a className="" href={`/article/${article.id}`}>
+          <Card.ImgOverlay>
+            <div className="overlay-style">
+              <Card.Title>
+                <div className="headline-text-style">{article.headline}</div>
+              </Card.Title>
+            </div>
+          </Card.ImgOverlay>
 
-        </Card.Body>
-        </Link>
+          <Card.Body></Card.Body>
+        </a>
       </Card>
-    
+     
     </div>
-    
   );
 };
 
