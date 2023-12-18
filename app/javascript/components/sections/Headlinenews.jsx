311e57e34a5cc6f4d9a4ac4be33f15d419449602
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-18 18:54:18 +0000

    Update Headlinenews.jsx

--- app/javascript/components/sections/Headlinenews.jsx
+++ app/javascript/components/sections/Headlinenews.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
+import '../../../assets/stylesheets/application.css';
 
 const Headlinenews = () => {
   const navigate = useNavigate();
@@ -22,54 +23,38 @@ const Headlinenews = () => {
   const allArticles = articles.map((article) => (
     <div className="row" key={article.id}>
       <div className="col-md-5">
-        <Link
-          to={`/article/${article.id}`}
+        <a
+          href={`/article/${article.id}`}
           className="text-start link-dark link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
         >
           <img
             src={article.image}
-            style={{ width: '100%', height: 'auto', margin: '0px, 0px, 10px, 0px', marginBottom: '15px' }}
-            className="img-fluid"
+            className="headline-image img-fluid"
             alt={article.headline}
           />
-        </Link>
+        </a>
       </div>
 
       <div className="col-md-7">
-        <Link
-          to={`/article/${article.id}`}
-          style={{ fontSize: '17px', fontWeight: '700', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '2', textOverflow: 'ellipsis', overflow: 'hidden'}}
-          className="text-start link-dark link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+        <a
+          href={`/article/${article.id}`}
+          className="link-headline-header line-clamp-2"
         >
           <div style={{}}>{article.headline}</div>
-        </Link>
-        <Link
-          to={`/article/${article.id}`}
-          style={{ fontSize: '15px', fontWeight: '400', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '2', textOverflow: 'ellipsis', overflow: 'hidden'}}
-          className="text-start link-dark link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+        </a>
+        <a
+          href={`/article/${article.id}`}
+          className="link-headline-news line-clamp-3"
         >
-          <div style={{}}>{article.main_text}</div>
-        </Link>
+          <div>{article.main_text}</div>
+        </a>
       </div>
-
-      {/* <div className="col-md-3">
-        <Link
-          to={`/article/${article.id}`}
-          style={{ fontSize: '7px', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '2', textOverflow: 'ellipsis', overflow: ''}}
-          className="text-start link-dark link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-        >
-          <div style={{}}>{article.headline}</div>
-        </Link>
-      </div> */}
-
     </div>
   ));
 
   const noArticles = (
-    <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
-      <h4>
-        Loading...
-      </h4>
+    <div className="centered-container">
+      <h4>Loading...</h4>
     </div>
   );
 
@@ -77,20 +62,6 @@ const Headlinenews = () => {
     <>
       <br />
       {articles.length > 0 ? <div>{allArticles}</div> : noArticles}
-      {/* <Card
-        style={{
-          backgroundColor: '#ffffff',
-          backgroundSize: 'cover',
-          color: 'black',
-          border: 0,
-          padding: '0 0 0 0',
-          margin: '0 0 0 0',
-        }}
-      >
-        <Card.Body>
-          {articles.length > 0 ? <div>{allArticles}</div> : noArticles}
-        </Card.Body>
-      </Card> */}
       <br />
     </>
   );
