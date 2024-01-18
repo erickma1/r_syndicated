1067cdd747d5846b5a26c3dbe9e34d2f99f8880a
erickma1 <110662611+erickma1@users.noreply.github.com>
2024-01-18 16:46:07 +0000

    Update Article.jsx

--- app/javascript/components/sections/Article.jsx
+++ app/javascript/components/sections/Article.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 const Article = () => {
   const params = useParams();
   const navigate = useNavigate();
-  const [article, setArticle] = useState({ main_text: "" });
+  const [article, setArticle] = useState({ main_text: "", headline: "", image: "" });
 
   useEffect(() => {
     const url = `/api/v1/show/${params.id}`;
@@ -24,67 +24,37 @@ const Article = () => {
   };
 
   const main_textList = () => {
-    let main_textList = "No main_text available";
+    let main_textListContent = "Story loading...";
 
     if (article.main_text.length > 0) {
-        main_textList = article.main_text
-        .split(",")
-        .map((main_text, index) => (
-          <li key={index} className="list-group-item">
-            {main_text}
-          </li>
-        ));
+      main_textListContent = (
+        <>
+       
+          <div className="link-section-headline line-clamp-3">
+            {article.headline}
+          </div>
+          <img
+          src={article.image}
+          style={{
+            width: '100%',
+            height: '170px',
+            margin: '0, 0, 10px, 0',
+            marginBottom: '15px',
+          }}
+          className="img-fluid"
+          alt={article.headline}
+        />
+          {article.main_text}
+        </>
+      );
     }
 
-    return main_textList;
+    return main_textListContent;
   };
 
   const articlemain_text = addHtmlEntities(article.main_text);
-  
-  return (
-    <div className="">
-      <div className="hero position-relative d-flex align-items-center justify-content-center">
-        <img
-          src={article.image}
-          alt={`${article.headline} image`}
-          className="img-fluid position-absolute"
-        />
-        <div className="overlay bg-dark position-absolute" />
-        <h1 className="display-4 position-relative text-white">
-          {article.headline}
-        </h1>
-      </div>
-      <div className="container py-5">
-        <div className="row">
-          <div className="col-sm-12 col-lg-3">
-            <ul className="list-group">
-              <h5 className="mb-2">Articles</h5>
-              {main_textList()}
-            </ul>
-          </div>
-          <div className="col-sm-12 col-lg-7">
-            <h5 className="mb-2">Preparation Instructions</h5>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: `${articlemain_text}`,
-              }}
-            />
-          </div>
-          {/* <div className="col-sm-12 col-lg-2">
-            <button
-              type="button"
-              className="btn btn-danger"
-            >
-              Delete Article
-            </button>
-          </div> */}
-        </div>
-        <Link to="/articles" className="btn btn-link">
-          Back to articles
-        </Link>
-      </div>
-    </div>
-  );
+
+  return <div className="">{main_textList()}</div>;
 };
 
 export default Article;
